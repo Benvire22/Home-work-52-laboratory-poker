02878225879884bff3267e24d2584ecca1b9eaf7
Limarev Ivan <ivantaketo@gmail.com>
2024-05-21 01:41:36 +0600

    Initial project: Homework - 52 poker

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => (
+    <>
+        <div className="playingCards faceImages">
+            <div className="cards">
+                <span className="card rank-k diams">
+                    <span className="rank">K</span>
+                    <span className="suit">♦</span>
+                </span>
+            </div>
+        </div>
+    </>
+);
+
+export default App;
